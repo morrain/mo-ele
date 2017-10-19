e344145f0736f361adaecd3f76f7651b0388ddca
morrain <626100676@qq.com>
2017-10-19 13:12:20 +0800

    先引入依赖库，再引入App模块，否则App中的样式会打包在material design 之前，导致App中的样式不生效

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-
 // 引入material design
 require('../node_modules/material-design-lite/material.min.css')
 require('../node_modules/material-design-lite/material.min.js')
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
 Vue.config.productionTip = false
 
