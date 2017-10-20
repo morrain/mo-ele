d518b92480851498f89ee1721a7187f53b44b2a9
morrain <626100676@qq.com>
2017-10-20 23:55:27 +0800

    改变meterial design的引入方式，合并到工程打包后，js会打包到主app中，导致样式有问题

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-// 引入material design
-require('../node_modules/material-design-lite/material.min.css')
-require('../node_modules/material-design-lite/material.min.js')
 
 import Vue from 'vue'
 import App from './App'
