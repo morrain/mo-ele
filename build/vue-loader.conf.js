53c89fc628febbe4b403f1a4f298294f4f947062
morrain <626100676@qq.com>
2017-10-24 15:29:00 +0800

    使用border-image绘制1px边框，解决高分屏适配问题

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -14,8 +14,12 @@ module.exports = {
     img: 'src',
     image: 'xlink:href'
   },
-  postcss: [require('autoprefixer')(), require('postcss-px-to-viewport')({
-    viewportWidth: 750, //根据视觉稿设置屏幕宽高 750px 是iphone 6 的尺寸。https://www.w3cplus.com/css/vw-for-layout.html
-    viewportHeight: 1334
-  })]
+  postcss: [
+    require('autoprefixer')(),
+    require('postcss-write-svg'),
+    require('postcss-px-to-viewport')({
+      viewportWidth: 750, //根据视觉稿设置屏幕宽高 750px 是iphone 6 的尺寸。https://www.w3cplus.com/css/vw-for-layout.html
+      viewportHeight: 1334
+    })
+  ]
 }
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -39,6 +39,37 @@ export default {
 
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
+<style scoped lang='scss'>
+
+/*https://www.w3cplus.com/css/vw-for-layout.html*/
+/*绘制上下边框，通过border-image参数控制绘制上或者下 或者上下边框*/
+@svg 1px-border-top-bottom {
+  height: 2px;
+  @rect {
+    fill: var(--color, black);
+    width: 100%;
+    height: 50%;
+  }
+}
+
+/*绘制左右边框，通过border-image参数控制绘制左或者右 或者左右边框*/
+@svg 1px-border-left-right {
+  width: 2px;
+  @rect {
+    fill: var(--color, black);
+    width: 50%;
+    height: 100%;
+  }
+}
+
+.md-subhead {
+  border: 1px solid transparent;
+  border-image: svg(1px-border-top-bottom param(--color red)) 2 0 2 0 stretch; //上下边框
+  border-image: svg(1px-border-top-bottom param(--color red)) 2 0 0 0 stretch; //上边框
+  border-image: svg(1px-border-top-bottom param(--color red)) 0 0 2 0 stretch; //下边框
+  border-image: svg(1px-border-left-right param(--color red)) 0 2 0 2 stretch; //左右边框
+  border-image: svg(1px-border-left-right param(--color red)) 0 0 0 2 stretch; //左边框
+  border-image: svg(1px-border-left-right param(--color red)) 0 2 0 0 stretch; //右边框
+}
 
 </style>
